e8e908041aa897c491a643500a7fe1430320454e
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-11-28 19:54:08 -0500

    [04-react-state-part-1] Blog taking user prompt example

--- 04-react-state-part-1/instructor/blog/src/App.js
+++ 04-react-state-part-1/instructor/blog/src/App.js
@@ -6,6 +6,19 @@ import Comment from './Comment.js';
 import Author from './Author.js';
 
 class Post extends Component {
+
+  state = {
+    body: 'Initial body'
+  }
+
+  changeBody = () => {
+    let newBody = prompt('What should the new body be?')
+
+    this.setState({
+      body: newBody
+    })
+  }
+
   render() {
 
     let allComments = this.props.post.comments.map((comment, index) =>
@@ -22,7 +35,9 @@ class Post extends Component {
 
         { allAuthors }
 
-        <p>{ this.props.post.body }</p>
+        <p>{ this.state.body }</p>
+
+        <button onClick={ this.changeBody }>Edit body</button>
 
         <h3>Comments</h3>
         { allComments }
