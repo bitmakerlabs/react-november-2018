45a5a4c1a66a7a5147099c7a33aef2d92a64681d
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-12-03 19:34:19 -0500

    [05-react-state-part-2] Convert components to Functional Components

--- 05-react-state-part-2/instructor/todo-list/src/ListItem.js
+++ 05-react-state-part-2/instructor/todo-list/src/ListItem.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+//
+// class ListItem extends Component {
+//   render() {
+//     return (
+//       <li>{ this.props.item }</li>
+//     );
+//   }
+// }
 
-class ListItem extends Component {
-  render() {
-    return (
-      <li>{ this.props.item }</li>
-    );
-  }
-}
+import React from 'react'
+
+// Functional Component
+const ListItem = props =>
+  <li>{ props.item }</li>
 
 export default ListItem
--- 05-react-state-part-2/instructor/todo-list/src/index.js
+++ 05-react-state-part-2/instructor/todo-list/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MyList from './MyList';
+import FunctionalMyList from './FunctionalMyList';
 import * as serviceWorker from './serviceWorker';
 
 const items = ['Buy Ice Cream', 'Eat Ice Cream', 'Go to the Gym']
 
-ReactDOM.render(<MyList items={ items } />, document.getElementById('root'));
+ReactDOM.render(<FunctionalMyList items={ items } />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
