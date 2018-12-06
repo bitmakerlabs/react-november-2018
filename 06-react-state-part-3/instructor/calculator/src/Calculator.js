790f20f9f702a8fc2706f5ff5e5f17f3e98f2df6
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-12-05 19:35:33 -0500

    [06-react-state-part-3] Add support for all operations

--- 06-react-state-part-3/instructor/calculator/src/Calculator.js
+++ 06-react-state-part-3/instructor/calculator/src/Calculator.js
@@ -5,7 +5,8 @@ class Calculator extends Component {
   state = {
     val1: 0,
     val2: 0,
-    sum: 0
+    result: 0,
+    operation: '+'
   }
 
   // Easy solution
@@ -16,15 +17,46 @@ class Calculator extends Component {
   //   this.setState({ sum })
   // }
 
-  // More advanced solution
-  handleKeyUp = (event) => {
+  calculate = () => {
+    const val1 = this.state.val1
+    const val2 = this.state.val2
+    let result = 0
+
+    switch(this.state.operation) {
+      case '+': {
+        result = val1 + val2
+        break
+      }
+      case '-': {
+        result = val1 - val2
+        break
+      }
+      case '*': {
+        result = val1 * val2
+        break
+      }
+      case '/': {
+        result = val1 / val2
+        break
+      }
+      default: {
+        console.log("ERROR! Invalid Operation")
+        break
+      }
+    }
+
+    this.setState({ result })
+  }
+
+  handleChangeVal = (event) => {
     this.setState({
       [event.target.name]: parseInt(event.target.value) || 0
-    }, () => {
-      this.setState({
-        sum: this.state.val1 + this.state.val2
-      })
-    })
+    }, this.calculate)
+  }
+
+  handleChangeOp = (event) => {
+    const operation = event.target.value
+    this.setState({ operation }, this.calculate)
   }
 
   render() {
@@ -33,11 +65,18 @@ class Calculator extends Component {
         <h1>Calculate with React!</h1>
 
         <div className="add">
-          <input type="text" name="val1" onKeyUp={ this.handleKeyUp } />
-          <span>+</span>
-          <input type="text" name="val2" onKeyUp={ this.handleKeyUp } />
+          <input type="text" name="val1" onKeyUp={ this.handleChangeVal } />
+
+          <select onChange={ this.handleChangeOp } >
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
+
+          <input type="text" name="val2" onKeyUp={ this.handleChangeVal } />
           <span>=</span>
-          <h3>{ this.state.sum }</h3>
+          <h3>{ this.state.result }</h3>
         </div>
       </div>
     )
