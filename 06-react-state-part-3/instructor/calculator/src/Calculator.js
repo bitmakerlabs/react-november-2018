f6fa233a832fd03907f5d162a2b10017244febd1
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-12-05 19:12:43 -0500

    [06-react-state-part-3] Handle addition

--- 06-react-state-part-3/instructor/calculator/src/Calculator.js
+++ 06-react-state-part-3/instructor/calculator/src/Calculator.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
 
+  state = {
+    val1: 0,
+    val2: 0,
+    sum: 0
+  }
+
+  // Easy solution
+  // handleKeyUp = (event) => {
+  //   const val1 = document.getElementById("val1").value
+  //   const val2 = document.getElementById("val2").value
+  //   const sum = (parseInt(val1) || 0) + (parseInt(val2) || 0)
+  //   this.setState({ sum })
+  // }
+
+  // More advanced solution
+  handleKeyUp = (event) => {
+    this.setState({
+      [event.target.name]: parseInt(event.target.value) || 0
+    }, () => {
+      this.setState({
+        sum: this.state.val1 + this.state.val2
+      })
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Calculate with React!</h1>
 
         <div className="add">
-          <input type="text" />
+          <input type="text" name="val1" onKeyUp={ this.handleKeyUp } />
           <span>+</span>
-          <input type="text" />
+          <input type="text" name="val2" onKeyUp={ this.handleKeyUp } />
           <span>=</span>
-          <h3></h3>
+          <h3>{ this.state.sum }</h3>
         </div>
       </div>
     )
