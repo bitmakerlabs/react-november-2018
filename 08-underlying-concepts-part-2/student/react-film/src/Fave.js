3dcd6b362af368841f342f3bf82eff799d396c42
abby <abby.greenbloom@gmail.com>
2018-12-16 20:57:17 -0500

    move solution

---
+++ 08-underlying-concepts-part-2/student/react-film/src/Fave.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Fave extends Component {
+  handleClick = (e) => {
+    e.stopPropagation()
+    console.log('Handling Fave Click!');
+    // Add this line. You'll call the function passed through props.
+    this.props.onFaveToggle();
+  }
+
+  render() {
+    const isFave = this.props.isFave ? 'remove_from_queue' : 'add_to_queue'
+
+    return (
+      <div className={ `film-row-fave ${ isFave }` } onClick={ this.handleClick }>
+        <p className="material-icons">{ isFave }</p>
+      </div>
+    );
+  }
+}
+
+export default Fave;
