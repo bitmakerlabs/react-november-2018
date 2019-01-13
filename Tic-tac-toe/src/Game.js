82441331a3eefd8831fcd056861448cf5eee55d0
abby <abby.greenbloom@gmail.com>
2019-01-13 15:55:19 -0500

    start tic tac toe

---
+++ Tic-tac-toe/src/Game.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Board from './Board';
+import './Game.css';
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
