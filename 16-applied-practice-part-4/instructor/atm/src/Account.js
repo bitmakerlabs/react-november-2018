7dbe13e3df73538a00985661b35f3d673695e486
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-21 19:31:31 -0500

    Add Account skeleton

--- 16-applied-practice-part-4/instructor/atm/src/Account.js
+++ 16-applied-practice-part-4/instructor/atm/src/Account.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
 class Account extends Component {
+
+  state = {
+    balance: 0
+  }
+
   render() {
+    let balanceClass = "balance"
+
+    if (this.state.balance === 0) {
+      balanceClass += " zero"
+    }
+
     return (
-      <h1>
-        {this.props.name}
-      </h1>
+      <div className="account">
+        <h2>
+          {this.props.name}
+        </h2>
+        <div className={balanceClass}>
+          {this.state.balance}
+        </div>
+        <input type="number" placeholder="Enter an amount" />
+        <input type="button" value="Deposit" />
+        <input type="button" value="Withdraw" />
+      </div>
     )
   }
 }
