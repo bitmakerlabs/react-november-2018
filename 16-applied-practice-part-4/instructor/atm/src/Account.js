fa8f85ceba2fedf5943e8e0419af3ce60254ba9d
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-21 19:50:24 -0500

    Add Deposit

--- 16-applied-practice-part-4/instructor/atm/src/Account.js
+++ 16-applied-practice-part-4/instructor/atm/src/Account.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react'
 class Account extends Component {
 
   state = {
-    balance: 0
+    balance: 0,
+    value: ''
+  }
+
+  handleOnChange = (e) => {
+    const value = e.target.value
+    this.setState({value})
+  }
+
+  handleDepositClick = (e) => {
+    let value = parseInt(this.state.value) || 0
+    value = value > 0 ? value : 0
+
+    this.setState({
+      balance: this.state.balance + value,
+      value: ''
+    })
   }
 
   render() {
@@ -21,8 +37,8 @@ class Account extends Component {
         <div className={balanceClass}>
           {this.state.balance}
         </div>
-        <input type="number" placeholder="Enter an amount" />
-        <input type="button" value="Deposit" />
+        <input type="number" placeholder="Enter an amount" value={ this.state.value } onChange={ this.handleOnChange } />
+        <input type="button" value="Deposit" onClick={ this.handleDepositClick } />
         <input type="button" value="Withdraw" />
       </div>
     )
