250e12e09d934c175c2c09bed9eddb3f1998c2aa
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-21 19:22:12 -0500

    Add logo

--- 16-applied-practice-part-4/instructor/atm/src/App.js
+++ 16-applied-practice-part-4/instructor/atm/src/App.js
@@ -1,27 +1,21 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from './assets/ga.png'
 import './App.css'
 
+import Account from './Account'
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>
-          src/App.js
-        </code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div id="content">
+    <div id="nav">
+      <div id="logo">
+        <img src={logo} alt="General Assembly Logo" />
+      </div>
+      <div id="title">
+        Bank of GA
+      </div>
+    </div>
+
+    <Account name="checking" />
   </div>
 )
 
